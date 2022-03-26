3032e89379ec79ac81fc80bd9530a29a129e9489
JosePozoSilva <jhosede23@gmail.com>
2022-03-26 17:34:42 -0400

    moviendo carpetas

--- MODAStore/src/app/app.component.ts
+++ MODAStore/src/app/app.component.ts
@@ -2,8 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  templateUrl: './HTML/app.component.html',
+  styleUrls:['./CSS/IndexStyle.css']
+
 })
 export class AppComponent {
   title = 'MODAStore';
