b4d949455e4075173c499e55f7805ac2972d5126
JosePozoSilva <jhosede23@gmail.com>
2022-03-28 01:42:30 -0400

    creación de página principal - parte 1 - navbar

--- MODAStore/src/app/app.component.ts
+++ MODAStore/src/app/app.component.ts
@@ -2,10 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './HTML/app.component.html',
-  styleUrls:['./CSS/IndexStyle.css']
+  templateUrl: './HTML/app.component-MenuNavBar.html',
+  styleUrls:['./CSS/app.component-Style-MenuNavBar.css']
 
 })
 export class AppComponent {
   title = 'MODAStore';
+  imgLogo = '/MODAStore/src/app/assets/logo-empresa.png';
 }
+5
---
+++ MODAStore/src/app/recap.ts
@@ -0,0 +1,2 @@
+//const usernamne: string = 'jhousef'; // se designa los tipos de varialbes con la orden de tipo de dato
+
