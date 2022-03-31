e2700af7b6f9a8a63d1aec2eb3d9dcf35c9e6b0a
mayu nina <mayumi>
2022-03-31 11:31:17 -0400

    actualizando carpetas

--- MODAStore/src/app/REGISTRO/registro_comprador/app.module.ts
+++ MODAStore/src/app/REGISTRO/registro_comprador/app.module.ts
@@ -2,7 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import { AppComponent } from '../app.component';
 import { HomeComponent } from './components/home/home.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { OffersComponent } from './components/offers/offers.component';
