1ce42a3b390a4e0eb879d6b06943480ac5792099
mayu nina <mayumi>
2022-03-31 14:04:08 -0400

    cargando la base de datos

--- MODAStore/src/app/services/auth.service.ts
+++ MODAStore/src/app/services/auth.service.ts
@@ -1,13 +1,18 @@
+import { DataApiMODASTOREService } from './data-api-modastore.service';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/observable';
 @Injectable({
   providedIn: 'root' 
 })
-export class AuthService {
+export class DataApiMODASTOREService {
+ Compradores: Observable<any>;
+ Comprador: Observable<any>;
+
+ 
 
   constructor(private http: HttpComprador) { }
  getAllComprador(){
-const url_api ="http://localhost:4200/api/modastore";
+const url_api ="http://localhost:3000/api/modastore";
 return this.http.get(url_api);
 
  } 
