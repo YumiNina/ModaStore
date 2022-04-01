6a3d0beb344d724ddebfd764a1d0e740d51543df
JosePozoSilva <jhosede23@gmail.com>
2022-04-01 17:47:39 -0400

    moviendo carpetas y creando footer and Inicio

--- MODAStore/src/app/app.module.ts
+++ MODAStore/src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 
 import { NavBar1Component } from './nav-bar1/nav-bar1.component';
+import { InicioComponent } from './inicio/inicio.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavBar1Component
+    NavBar1Component,
+    InicioComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule
