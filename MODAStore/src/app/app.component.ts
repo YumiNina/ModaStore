9ffdfbe9c3057b1ab086258390f5e4bc000557bb
JosePozoSilva <jhosede23@gmail.com>
2022-04-02 14:29:36 -0400

    ordenando el archivo modules.ts

--- MODAStore/src/app/app.component.ts
+++ MODAStore/src/app/app.component.ts
@@ -3,8 +3,8 @@ import { RouterModule, Router, NavigationEnd } from '@angular/router';
 ///import { NgxSpinnerService } from 'ngx-spinner';
 @Component({
     selector: 'app-root',
-    templateUrl: './component/nav-bar1/nav-bar1.component.html',
-    styleUrls: ['./component/nav-bar1/nav-bar1.component.css']
+    templateUrl: './component/inicio/inicio.component.html',
+    styleUrls: ['./component/inicio/inicio.component.css']
 })
 export class AppComponent {
     title = 'MODAStore';
