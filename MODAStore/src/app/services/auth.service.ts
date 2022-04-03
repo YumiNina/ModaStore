5c6cb712bfa0bbfcd0b295c6b6561a3b4dd729b5
mayu nina <mayumi>
2022-04-02 23:19:06 -0400

    corrigiendo errores

--- MODAStore/src/app/services/auth.service.ts
+++ MODAStore/src/app/services/auth.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import {map } from 'rxjs/operators';
-import { Login3 } from './../component/comprador/login3/login3.component';
+//import { Login3 } from './../component/comprador/login3/login3.component';
 import { isNullOrUndefined } from 'util';
 
 @Injectable({
@@ -33,13 +33,13 @@ return this.http
 )
 .pipe(map(data => data));
 }
-Login3(ci:number, contraseña:string): Observable<any>{
+/*Login3(ci:number, contraseña:string): Observable<any>{
   const url_api = http://localhost:3000/api/users/login;
   return this.http.post(url_api,{ci, contraseña},
     {Headers: this.headers}
     ).pipe(map(data => data ));
 }
-
+*/
 setUser(user){
   let user_string = JSON.stringify(user);
   localStorage.setItem("currentUser", user_string);
@@ -49,7 +49,7 @@ setToken(token){
 localStorage.setItem("accessToken", token);
 }
 
- getToken(){
+getToken(){
 return localStorage.getItem("accessToken"); 
 
  }
@@ -95,6 +95,26 @@ function setToken() {
 }
 
 function getToken() {
+  throw new Error('Function not implemented.');
+}
+
+function setUser(user: any) {
+  throw new Error('Function not implemented.');
+}
+
+function user(user: any) {
+  throw new Error('Function not implemented.');
+}
+
+function token(token: any) {
+  throw new Error('Function not implemented.');
+}
+
+function getCurrentUser() {
+  throw new Error('Function not implemented.');
+}
+
+function logoutUser() {
   throw new Error('Function not implemented.');
 }
 
