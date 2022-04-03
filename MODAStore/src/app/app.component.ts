b22a8d0fb1e880a9d9c812ba0ffb3485358240f6
JosePozoSilva <jhosede23@gmail.com>
2022-04-03 18:55:24 -0400

    Subiendo formulario para registro de administradores primarios

--- MODAStore/src/app/app.component.ts
+++ MODAStore/src/app/app.component.ts
@@ -7,11 +7,21 @@ import { RouterModule, Router, NavigationEnd } from '@angular/router';
   //  templateUrl: './app.component.html',
   //  styleUrls: ['./app.component.css'],
 
+
+  /*
       templateUrl: './component/inicio/inicio.component.html',
       styleUrls: ['./component/inicio/inicio.component.css']
 
+      */
+
+      templateUrl: './component/admin/principal/registro1/registro1.component.html',
+      styleUrls: ['./component/admin/principal/registro1/registro1.component.css']
+
 })
+
 export class AppComponent {
+
     title = 'MODAStore';
 
 }
+
