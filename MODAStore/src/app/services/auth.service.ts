2b31c47b54955404cefd584cc17221d11303a8c4
mayu nina <mayumi>
2022-04-04 08:31:18 -0400

    modificando auth-service

--- MODAStore/src/app/services/auth.service.ts
+++ MODAStore/src/app/services/auth.service.ts
@@ -1,32 +1,18 @@
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
-<<<<<<< HEAD
 import { map } from 'rxjs/operators';
 //import { Login3 } from './../component/comprador/login3/login3.component';
 //import { isNullOrUndefined } from 'util';
 import { UserInterface } from '../models/user-interface';
-=======
-import {map } from 'rxjs/operators';
->>>>>>> f594d68be658cfab37e24a3c3ff4fcd933a28070
-
-
-
 /*
-
-
 //parte comentada
 //import { Login3 } from './../component/comprador/login3/login3.component';
-
-
-
-
-
-
 /*
 import { UserInterface } from '../models/logincomprador-interface';
-import { UserInterface } from '../models/user-interface';
-*/
+import { UserInterface } from '../models/user-interface';*/
+import { Registro3Component } from './../component/comprador/registro3/registro3.component';
+
 @Injectable({
 
   providedIn: 'root'
@@ -38,28 +24,37 @@ export class AuthService {
   headers: HttpHeaders = new HttpHeaders({
     "Content-Type": " application/json"
   });
-<<<<<<< HEAD
-/*
-  Registro3(nombre: string, apellido: string, ci: number, contraseña: string, email: string, celular: number, ciudad: string, direccion: string)
-=======
 
+  compradorregistro3(nombre: string, apellido: string, ci: number, contraseña: string, email: string, celular: number, ciudad: string, direccion: string){
+    const url_api = "http://localhost:3000/api/registro3";
+    return this.http
+      .post<UserInterface>(
+        url_api,
+        {
+          nombre: nombre,
+          apellido: apellido,
+          ci: ci,
+          contraseña:contraseña,
+          email:email,
+          celular:celular,
+          ciudad:ciudad,
+          direccion:direccion
+        },
+        { headers: this.headers }
+      )
+      .pipe(map(data => data));
 
-
-
-  //parte comentada
-  //Registro3(id: String, nombre: string, apellido:string, ci:number, contrasena:string, email: string, celular:number, ciudad:string, direccion:string)
->>>>>>> f594d68be658cfab37e24a3c3ff4fcd933a28070
+  }
 }
+
+
+
+
 /*
-
-
-
-
 
 //parte comentada
 
 getAllComprador() {
-<<<<<<< HEAD
   const url_api = "http://localhost:4200/api/modastore";
   return this.http
     .post<UserInterface>(url_api, {
@@ -75,25 +70,6 @@ getAllComprador() {
       { Headers: this.headers }
     )
     .pipe(map(data => data));
-=======
-
-
-  const url_api ="http://localhost:4200/api/modastore";
-  return this.http.post<UserInterface>(url_api, {
-  nombre:nombre,
-  apellido:apellido,
-  ci:ci,
-  contraseña:contraseña,
-  email:email,
-  celular:celular,
-  ciudad:ciudad,
-  direccion:direccion
- },
-{Headers: this.headers}
-)
-.pipe(map(data => data));
-
->>>>>>> f594d68be658cfab37e24a3c3ff4fcd933a28070
 }
 
 */
@@ -107,18 +83,11 @@ Login3(ci:number, contraseña:string): Observable<any>{
     ).pipe(map(data => data ));
 }
 
-<<<<<<< HEAD
 */
 /*
 setUser(user: UserInterface): void {
   let user_String = JSON.stringify(user);
   localStorage.setItem("currentUser", user_string);
-=======
-
-setUser(user UserInterface): void{
-  let_user_String = JSON.stringify(user),
-  localStorage.SetItem("currentUser", user_string)
->>>>>>> f594d68be658cfab37e24a3c3ff4fcd933a28070
 }
 
 setToken(token){
@@ -126,22 +95,8 @@ setToken(token){
 }
 
 getToken(){
-<<<<<<< HEAD
   return localStorage.getItem("accessToken");
 
-=======
-return localStorage.getItem("accessToken");
-
- }
-
-getCurrentUser(): UserInterface{
-let user_string = localStorage.getItem("currentUser");
-if(isNullOrUndefined(user_string)){
-let user = JSON.parse(user_string);
-return user;
-} else{
-  return null
->>>>>>> f594d68be658cfab37e24a3c3ff4fcd933a28070
 }
 getCurrentUser(): UserInterface {
   let user_string = localStorage.getItem("currentUser");
@@ -157,69 +112,7 @@ logoutUser(){
   const url_api = 'http:localhost:3000/api/Users/logout?access_token=${access_token}';
   localStorage.removeItem('accessToken')
   localStorage.removeItem('currentUser')
-<<<<<<< HEAD
   return this.http.post<UserInterface>(url_api, { Headers: this.headers });
-=======
-  return this.http.post(url_api, {Headers: this.headers});
-}
-*/
-
-
-
-
-
-
-
-
-
-
-function getAllComprador() {
-  throw new Error('Function not implemented.');
-}
-
-function Login3(ci: any, number: any, contraseña: any, string: any) {
-  throw new Error('Function not implemented.');
-}
-
-function ci(ci: any, number: any, contraseña: any, string: any) {
-  throw new Error('Function not implemented.');
-}
-
-function contraseña(ci: any, number: any, contraseña: any, string: any) {
-  throw new Error('Function not implemented.');
-}
-
-function url_api(url_api: any) {
-  throw new Error('Function not implemented.');
-}
-
-function setToken() {
-  throw new Error('Function not implemented.');
-}
-
-function getToken() {
-  throw new Error('Function not implemented.');
-}
-
-function setUser(user: any) {
-  throw new Error('Function not implemented.');
-}
-
-function user(user: any) {
-  throw new Error('Function not implemented.');
-}
-
-function token(token: any) {
-  throw new Error('Function not implemented.');
-}
-
-function getCurrentUser() {
-  throw new Error('Function not implemented.');
-}
-
-function logoutUser() {
-  throw new Error('Function not implemented.');
->>>>>>> f594d68be658cfab37e24a3c3ff4fcd933a28070
 }
 
 function user_string(arg0: string, user_string: any) {
