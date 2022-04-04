2973b909786ff026a7c2e7742af547c27324eb4c
AracelyCondoriTipola <aracely96.ac>
2022-04-03 20:07:59 -0400

    slider-hombre

--- MODAStore/src/app/app.component.ts
+++ MODAStore/src/app/app.component.ts
@@ -7,8 +7,11 @@ import { RouterModule, Router, NavigationEnd } from '@angular/router';
   //  templateUrl: './app.component.html',
   //  styleUrls: ['./app.component.css'],
 
-      templateUrl: './component/inicio/inicio.component.html',
-      styleUrls: ['./component/inicio/inicio.component.css']
+      //templateUrl: './component/inicio/inicio.component.html',
+     // styleUrls: ['./component/inicio/inicio.component.css']
+
+     templateUrl: './component/hombre/hombre.component.html',
+     styleUrls: ['./component/hombre/hombre.component.css']
 
 })
 export class AppComponent {
