aeb41e76e4ae50f5be60618139f43df7d73ae080
mayu nina <mayumi>
2022-04-04 08:50:22 -0400

    modificando auth-service

--- MODAStore/src/app/services/auth.service.ts
+++ MODAStore/src/app/services/auth.service.ts
@@ -2,16 +2,8 @@ import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs/internal/observable';
 import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { map } from 'rxjs/operators';
-//import { Login3 } from './../component/comprador/login3/login3.component';
-//import { isNullOrUndefined } from 'util';
 import { UserInterface } from '../models/user-interface';
-/*
-//parte comentada
-//import { Login3 } from './../component/comprador/login3/login3.component';
-/*
-import { UserInterface } from '../models/logincomprador-interface';
-import { UserInterface } from '../models/user-interface';*/
-import { Registro3Component } from './../component/comprador/registro3/registro3.component';
+import { isNullOrUndefined } from 'util';
 
 @Injectable({
 
@@ -54,10 +46,37 @@ export class AuthService {
       )
       .pipe(map(data => data));
   }
+  setUser(comprador: UserInterface): void {
+    let comprador_string = JSON.stringify(comprador);
+    localStorage.setItem("currentUser", comprador_string);
+  }
+  setToken(token): void {
+    localStorage.setItem("accessToken", token);
+  }
 
+  getToken() {
+    return localStorage.getItem("accessToken");
+  }
 
+  getCurrentComprador(): UserInterface {
+    let comprador_string = localStorage.getItem("currentUser");
+    if (!isNullOrUndefined(comprador_string)) {
+      let comprador: UserInterface = JSON.parse(comprador_string);
+      return comprador;
+    } else {
+    //  return null;
+
+    }
+  }
+
+  logoutComprador() {
+    let accessToken = localStorage.getItem("accessToken");
+    const url_api = `http://localhost:3000/api/Users/logout?access_token=${accessToken}`;
+    localStorage.removeItem("accessToken");
+    localStorage.removeItem("currentUser");
+    return this.http.post<UserInterface>(url_api, { headers: this.headers });
+  }
 }
-
 
 
 
@@ -86,46 +105,4 @@ getAllComprador() {
 */
 //parte comentada
 
-/*
-Login3(ci:number, contraseña:string): Observable<any>{
-  const url_api = http://localhost:3000/api/users/login;
-  return this.http.post<UserInterface>(url_api,{ci, contraseña},
-    {Headers: this.headers}
-    ).pipe(map(data => data ));
-}
 
-*/
-/*
-setUser(user: UserInterface): void {
-  let user_String = JSON.stringify(user);
-  localStorage.setItem("currentUser", user_string);
-}
-
-setToken(token){
-  localStorage.setItem("accessToken", token);
-}
-
-getToken(){
-  return localStorage.getItem("accessToken");
-
-}
-getCurrentUser(): UserInterface {
-  let user_string = localStorage.getItem("currentUser");
-  if (isNullOrUndefined(user_string)) {
-    let user: UserInterface = JSON.parse(user_string);
-    return user;
-  } else {
-    return null
-  }
-}
-logoutUser(){
-  let accessToken = localStorage.getItem('accessToken')
-  const url_api = 'http:localhost:3000/api/Users/logout?access_token=${access_token}';
-  localStorage.removeItem('accessToken')
-  localStorage.removeItem('currentUser')
-  return this.http.post<UserInterface>(url_api, { Headers: this.headers });
-}
-
-function user_string(arg0: string, user_string: any) {
-  throw new Error('Function not implemented.');
-}*/
