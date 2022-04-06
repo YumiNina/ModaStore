b5a7ac765620e932f6a49ee2d34947f8796811fd
AracelyCondoriTipola <aracely96.ac>
2022-04-06 16:27:28 -0400

    q

---
+++ MODAStore/src/app/component/deptos/la-paz/la-paz.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-la-paz',
+  templateUrl: './la-paz.component.html',
+  styleUrls: ['./la-paz.component.css']
+})
+export class LaPazComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
